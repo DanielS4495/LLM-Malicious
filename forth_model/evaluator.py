a454499caf8e3f596d1f6448f003d408ccd98946
SirAriela <arielag1998@gmail.com>
2026-05-04 11:26:28 +0300

    rearange files

--- forth_model/evaluator.py
+++ forth_model/evaluator.py
@@ -36,7 +36,7 @@ load_dotenv()
 #          "gemini", "perplexity", "huggingface", "gpt", "openrouter", "ollama", "MISTRAL"
 CURRENT_PROVIDER = "groq_llama-3.1"
 
-INPUT_FILE = "responses_results_mistral-small-latest_1.csv"
+INPUT_FILE = "results/responses_results_mistral-small-latest_1.csv"
 TARGET_MODEL_NAME = "MISTRAL_small-latest"
 SAFE_NAME = TARGET_MODEL_NAME.replace('/', '-')
 FINAL_OUTPUT_FILE = f"EVALUATE_{SAFE_NAME}_{CURRENT_PROVIDER}_final.csv"
